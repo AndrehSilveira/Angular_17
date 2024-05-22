37b6e4fa02b2dc35c039eadee90f94a689b72c6b
André Silveira <andresilveira.email@gmail.com>
2024-05-21 22:18:32 -0300

    Initial commit

---
+++ angular-basico/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from "./componente-01/componente-01.component";
+import { Componente02Component } from "./componente02/componente02.component";
+import { Componente03Component } from "./componente03/componente03.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component]
+})
+export class AppComponent {
+  title = 'angular-basico';
+}
---
+++ angular-basico/src/app/componente03/componente03.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente03',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente03.component.html',
+  styleUrl: './componente03.component.css'
+})
+export class Componente03Component {
+
+    // Variável recebendo a imagem
+    imagem:string = 'assets/dia.jpeg';
+
+    // Função para alterar a imagem
+    alterarImagem(){
+      if(this.imagem === 'assets/dia.jpeg'){
+        this.imagem = 'assets/noite.jpeg';
+      }else{
+        this.imagem = 'assets/dia.jpeg';
+      }
+    }
+}
