f132eb627b7d2ca265187866dc052ed3a649027d
André Silveira <andresilveira.email@gmail.com>
2024-05-24 23:26:30 -0300

    Diretivas ngIf e ngFor

--- angular-basico/src/app/app.component.ts
+++ angular-basico/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { Componente01Component } from "./componente-01/componente-01.component";
 import { Componente02Component } from "./componente02/componente02.component";
 import { Componente03Component } from "./componente03/componente03.component";
+import { Componente04Component } from "./componente04/componente04.component";
+import { Componente05Component } from "./componente05/componente05.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component]
+    imports: [RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component]
 })
 export class AppComponent {
   title = 'angular-basico';
