b09ce4edb163e9b8b3553b24a9faa4fda4df3c2c
André Silveira <andresilveira.email@gmail.com>
2024-06-13 21:42:15 -0300

    Formulário finalizado

--- angular-basico/src/app/componente11/componente11.component.ts
+++ angular-basico/src/app/componente11/componente11.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Pessoa } from '../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-componente11',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
@@ -23,5 +24,78 @@ export class Componente11Component {
 
   //Vetor
   vetor:Pessoa[] = [];
+
+  //Armazenar indice
+  indice:number = -1;
+
+  cadastrar(){
+
+    //Cadastro do vetor
+    this.vetor.push(this.formulario.value as Pessoa);
+
+    //Limpeza dos inputs
+    this.formulario.reset();
+
+    //visualização no console
+    //console.table(this.vetor);
+
+  }
+
+  //Função de seleção
+  selecionar(indice:number){
+
+    //Atribuir o indice da pessoa
+    this.indice = indice;
+
+    //Atribuir os dados da pessoa no formulário
+    this.formulario.setValue({
+      nome   : this.vetor[indice].nome,
+      idade  : this.vetor[indice].idade,
+      cidade : this.vetor[indice].cidade
+    });
+
+    //Visibilidade dos botões
+    this.btnCadastrar = false;
+  }
+
+  // Função de alteração
+  alterar(){
+
+    //Alterar o vetor
+    this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+    // Limpar os inputs
+    this.formulario.reset();
+
+    // Visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+  // Função de remoção
+  remover(){
+
+    //Removendo pessoa do vetor
+    this.vetor.splice(this.indice, 1);
+
+    //Limpeza dos inputs
+    this.formulario.reset();
+
+    //Visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+  // Função de cancelamento
+  cancelar(){
+
+    // Limpeza dos inputs
+    this.formulario.reset();
+
+    // Visibilidade dos botões
+    this.btnCadastrar = true;
+  }
+
+
+
+  
 
 }
